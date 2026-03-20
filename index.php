4bef6476d7a5222ce29d10bf606dbeb81265e145
itim-wonranon <itemwonranon@gmail.com>
2026-03-20 20:21:20 +0700

    แก้ใหม่

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ระบบบริหารจัดการโรงเรียนสาธิตวิทยาd - Dashboard</title>
+    <title>ระบบบริหารจัดการโรงเรียนสาธิตวิทยา - Dashboard</title>
 
     <!-- Favicon -->
     <link rel="icon" type="image/svg+xml" href="images/favicon.svg">
